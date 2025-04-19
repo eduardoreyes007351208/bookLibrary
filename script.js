144d8465813d1ec7064640325a34cbe4acd9d03f
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-04-18 17:42:05 -0700

    feat: Create functions to add and delete books
    Create function that adds books to the array and
    displays the books. Also add delete button that deletes
    the book from the array and displays the new array.

--- script.js
+++ script.js
@@ -1,51 +1,86 @@
 /* initialize variables */
 const myLibrary = [];
-const addButton = document.querySelector('#addButton')
-const closeButton = document.querySelector('#closeButton')
-const submitButton = document.querySelector('#submitButton')
-const bookFormCont = document.querySelector('#bookForm')
-const titleInput = document.querySelector('#title')
-const authorInput = document.querySelector('#author')
-const pagesInput = document.querySelector('#pages')
-const readInput = document.querySelector('#read')
-
+const addButton = document.querySelector("#addButton");
+const closeButton = document.querySelector("#closeButton");
+const submitButton = document.querySelector("#submitButton");
+const bookFormCont = document.querySelector("#bookForm");
+const bookContainer = document.querySelector("#libraryDiv");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const readInput = document.querySelector("#read");
 
 /* initialize the book constructor */
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.uuid = crypto.randomUUID()
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.uuid = crypto.randomUUID();
 }
 
+let displayCards = () => {
+  bookContainer.innerHTML = myLibrary
+    .map(
+      (book) =>
+        `<div>
+            <h4 class='bookTitle'>Title: ${book.title}</h4>
+            <h4>Author: ${book.author}</h4>
+            <h4>Pages: ${book.pages}</h4>
+            <h4>Read: ${book.read}</h4>
+            <button id='${book.uuid}' class='deleteButtons'>Delete</button>
+        </div>`
+    )
+    .join("");
+    deleteBook()
+};
 
 let addBook = (title, author, pages, read) => {
-    console.log(title, author, pages, read)
-    let newBook = new Book(title, author, pages, read)
-    myLibrary.push(newBook)
-    console.log(myLibrary)
+  console.log(title, author, pages, read);
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+};
+
+let removeBook = (arr, id) => {
+    const objIdIndex = arr.findIndex((obj) => obj.id === id);
+    arr.splice(objIdIndex, 1);
+    return arr;
 }
-addButton.addEventListener('click', (event) => {
-    event.preventDefault()
-    bookFormCont.style.visibility = 'visible';
-})
-closeButton.addEventListener('click', (event) => {
-    event.preventDefault()
-    bookFormCont.style.visibility = 'hidden';
-})
-submitButton.addEventListener('click', (event) => {
-    event.preventDefault()
-    let bTitle = titleInput.value;
-    let bAuthor = authorInput.value;
-    let bPages = pagesInput.value;
-    let bRead = readInput.checked;
-    addBook(bTitle, bAuthor, bPages, bRead)
-    /* input reset */
-    titleInput.value = '';
-    authorInput.value = '';
-    pagesInput.value = '';
-    readInput.checked = false;
-    /* make hidden */
-    bookFormCont.style.visibility = 'hidden';
-})
+
+let deleteBook = () => {
+  const deleteButtons = document.querySelectorAll(".deleteButtons");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      console.log(removeBook(myLibrary, button.id));
+      displayCards()
+    });
+  });
+};
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  bookFormCont.style.visibility = "visible";
+});
+closeButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  bookFormCont.style.visibility = "hidden";
+});
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  let bTitle = titleInput.value;
+  let bAuthor = authorInput.value;
+  let bPages = pagesInput.value;
+  let bRead = readInput.checked;
+  addBook(bTitle, bAuthor, bPages, bRead);
+  /* input reset */
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  readInput.checked = false;
+  /* make hidden */
+  bookFormCont.style.visibility = "hidden";
+  displayCards();
+});
+
+addBook("The Hunger Games", "Suzanne Collins", "374", true);
+displayCards();
